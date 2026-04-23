24f371f5663b4fa437ecae6bd18d6919012f6715
abdennour <abdennourkhiari6@gmail.com>
2026-04-23 13:26:37 +0100

    abdou auth

--- app/src/main/java/com/example/saveyournotes/CreateAccountActivity.java
+++ app/src/main/java/com/example/saveyournotes/CreateAccountActivity.java
@@ -1,12 +1,23 @@
 package com.example.saveyournotes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;import android.widget.Toast;
 
 public class CreateAccountActivity extends AppCompatActivity {
 
@@ -15,10 +26,39 @@ public class CreateAccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_create_account);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+            EditText email =findViewById(R.id.editTextTextEmailAddress2);
+            EditText password =findViewById(R.id.editTextTextPassword);
+            Button create =findViewById(R.id.button5);
+            ImageButton retour =findViewById(R.id.imageButton2);
+            FirebaseAuth auth;
+            auth= FirebaseAuth.getInstance();
+            retour.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent=new Intent(CreateAccountActivity.this, LoginActivity.class);
+                    startActivity(intent);
+
+                }
+            });
+       create.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               String eml =email.getText().toString();
+               String psw =password.getText().toString();
+               auth.createUserWithEmailAndPassword(eml,psw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                   @Override
+                   public void onComplete(@NonNull Task<AuthResult> task) {
+                       if(task .isSuccessful()){
+                           Intent intent =new Intent(CreateAccountActivity.this,HomeActivity.class);
+                           startActivity(intent);
+                       } else {
+                           String error = task.getException().getMessage();
+                           Toast.makeText(CreateAccountActivity.this, error, Toast.LENGTH_LONG).show();
+                       }
+                   }
+               });
+           }
+       });
     }
 }
--- app/src/main/java/com/example/saveyournotes/LoginActivity.java
+++ app/src/main/java/com/example/saveyournotes/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.example.saveyournotes;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,10 +23,33 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.login_activity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        FirebaseAuth auth2;
+        auth2 = FirebaseAuth.getInstance();
+        Button btn = findViewById(R.id.button3);
+        EditText email2=findViewById(R.id.editTextTextEmailAddress3);
+        EditText password2=findViewById(R.id.editTextTextPassword2);
+        Button sign= findViewById(R.id.buttonsignup);
+         sign.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View v) {
+                 Intent intent =new Intent(LoginActivity.this,CreateAccountActivity.class);
+                 startActivity(intent);
+             }
+         });
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String eml2= email2.getText().toString();
+                String psw2= password2.getText().toString();
+                auth2.signInWithEmailAndPassword(eml2,psw2).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Intent intent=new Intent(LoginActivity.this,HomeActivity.class);
+                        startActivity(intent);
+                    }
+                });
+            }
         });
+
     }
 }
